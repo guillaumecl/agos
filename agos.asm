29d248dbf67c63dd79a02b2ac600335fcf3a7124
Guillaume Clement <gclement@baobob.org>
2014-03-02 22:49:20 +0100

    Show the palette on the screen, and add a transparency option to bitmap drawing.

--- agos.asm
+++ agos.asm
@@ -6,16 +6,18 @@ section .data
 framebuffer: dd 0
 flip_func: dd 0
 
-bitmap:	db 0, 0, 0, 0, 2, 2, 0, 0, 0, 0
-		db 0, 0, 0, 2, 3, 3, 2, 0, 0, 0
-		db 0, 0, 2, 3, 4, 4, 3, 2, 0, 0
-		db 0, 2, 3, 4, 5, 5, 4, 3, 2, 0
-		db 2, 3, 4, 5, 6, 6, 5, 4, 3, 2
-		db 2, 3, 4, 5, 6, 6, 5, 4, 3, 2
-		db 0, 2, 3, 4, 5, 5, 4, 3, 2, 0
-		db 0, 0, 2, 3, 4, 4, 3, 2, 0, 0
-		db 0, 0, 0, 2, 3, 3, 2, 0, 0, 0
-		db 0, 0, 0, 0, 2, 2, 0, 0, 0, 0
+%assign ffh 0xff
+
+bitmap:	db ffh, ffh, ffh, ffh, 02h, 02h, ffh, ffh, ffh, ffh
+		db ffh, ffh, ffh, 02h, 03h, 03h, 02h, ffh, ffh, ffh
+		db ffh, ffh, 02h, 03h, 04h, 04h, 03h, 02h, ffh, ffh
+		db ffh, 02h, 03h, 04h, 05h, 05h, 04h, 03h, 02h, ffh
+		db 02h, 03h, 04h, 05h, 06h, 06h, 05h, 04h, 03h, 02h
+		db 02h, 03h, 04h, 05h, 06h, 06h, 05h, 04h, 03h, 02h
+		db ffh, 02h, 03h, 04h, 05h, 05h, 04h, 03h, 02h, ffh
+		db ffh, ffh, 02h, 03h, 04h, 04h, 03h, 02h, ffh, ffh
+		db ffh, ffh, ffh, 02h, 03h, 03h, 02h, ffh, ffh, ffh
+		db ffh, ffh, ffh, ffh, 02h, 02h, ffh, ffh, ffh, ffh
 
 
 %ifdef ELF
@@ -26,23 +28,44 @@ agos:
 		mov [framebuffer], edi
 		mov [flip_func], ebp
 
-		;; fill the screen with a 02 color
-		cld
-		mov ecx, 320*200/4
-		mov eax, 0x00000000
-		rep stosd
+		mov ecx, 200
+		xor al,al
+fill_screen:
+		mov byte [edi], al
+		inc edi
+		inc al
+		jnz fill_screen
+		add edi, 320-256
+		dec ecx
+		jnz fill_screen
+
 
 		mov esi, bitmap
 		mov ecx, 10
 		mov edx, 10
-		mov edi, [framebuffer]
-		add edi, 320*90+150
-		call put_bitmap
+		mov ah, 10
+		mov al, 20
+		call compute_pos
+		call put_bitmap_transp
 
 		call [flip_func]
 
 		ret
 
+; compute a buffer position
+; top eax must be zeroed
+; al: x
+; ah: y
+; result in edi, modifications: eax and edi
+compute_pos:
+		movzx edi, al	; edi = x
+		movzx eax, ah
+		shl eax, 6
+		add edi, eax	; edi = x + 64*y
+		shl eax, 2
+		add edi, eax	; edi = x + 64*y + 256*y
+		add edi, [framebuffer]
+		ret
 
 
 ; edi: bitmap destination
@@ -60,5 +83,33 @@ put_bitmap:
 		add edi, 320
 		dec edx
 		jnz .line_loop
+
+		ret
+
+; Draw a bitmap with some transparency (0xff is transparent)
+; edi: bitmap destination
+; esi: the bitmap
+; ecx: width (pixels)
+; edx: height (pixels)
+put_bitmap_transp:
+		mov ebp, ecx
+
+.line_loop:
+		mov al, byte [esi]
+		cmp al, 0xff
+		je skip
+		movsb
+		jmp next
+skip:
+		inc esi
+		inc edi
+next:	dec ecx
+		jnz put_bitmap_transp.line_loop
+		mov ecx, ebp
+
+		sub edi, ecx
+		add edi, 320
+		dec edx
+		jnz put_bitmap_transp.line_loop
 
 		ret
